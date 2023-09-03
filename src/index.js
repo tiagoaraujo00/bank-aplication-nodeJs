da8643d9045e52c6a8cc04c6bbe18f53c247c842
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-09-03 14:02:03 -0300

    adicionando o README com requisitos e regras de negócio

--- src/index.js
+++ src/index.js
@@ -4,13 +4,4 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/courses', (_request, response) => {
-  return response.json(['Curso 01', 'Curso 02', 'Curso 03'])
-})
-app.post('/courses', (request, response) => {
-  const { body } = request
-  console.log(body);
-  return response.json(['Curso 01', 'Curso 02', 'Curso 03', 'Curso 04'])
-})
-
 app.listen(3333, () => console.log('ouvindo na porta 3333'))
