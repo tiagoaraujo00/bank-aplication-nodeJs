c762fcc4ff1df0eaaf1ffe7fce3b19bfb12c14e4
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-09-03 15:55:14 -0300

    feat: account req-01

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+const customers = []
+
 app.post('/account', (req, res) => {
-  const { cpf, name } = request.body
+  const id = uuidv4()
+  const { cpf, name } = req.body
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  })
+  return res.status(201).send()
 })
 
 app.listen(3333, () => console.log('ouvindo na porta 3333'))
