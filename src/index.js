c5708c5e774d8ec1068fbb51e671ec4fdfe4cc3f
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-09-03 12:14:41 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/courses', (_request, response) => {
+  return response.json(['Curso 01', 'Curso 02', 'Curso 03'])
+})
+app.post('/courses', (_request, response) => {
+  return response.json(['Curso 01', 'Curso 02', 'Curso 03', 'Curso 04'])
+})
+
+app.listen(3333, () => console.log('ouvindo na porta 3333'))
