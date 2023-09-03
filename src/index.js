8b59338e0854d8e669ead5e817fc51c807103ff5
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-09-03 14:59:59 -0300

    uuid install

--- src/index.js
+++ src/index.js
@@ -4,4 +4,8 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.post('/account', (req, res) => {
+  const { cpf, name } = request.body
+})
+
 app.listen(3333, () => console.log('ouvindo na porta 3333'))
