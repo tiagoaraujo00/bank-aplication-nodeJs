4abf8c89960779341af1b2b3e8a3c0515481bb08
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-09-04 20:38:17 -0300

    fix: correções necessárias

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const autenticationAccountByCpf = (req, res, next) => {
   req.customer = customer
   return next()
 }
-app.get("/statement/", autenticationAccountByCpf, (req, res) => {
+app.get("/statement", autenticationAccountByCpf, (req, res) => {
   const { customer } = req
   return res.json(customer.statement)
 })
